a408a83fef8f976e8014bc34885658136fabdbc1
ruthless2skech <74581915+manesh1234@users.noreply.github.com>
2023-01-28 17:43:02 +0530

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -15,7 +15,8 @@ const Body = () => {
     const [filteredRestaurants, setFilteredRestaurants] = useState([]);
     const [allRestaurants, setAllRestaurants] = useState([]);
     const [searchText, setSearchText] = useState('');
-    const [location,setlocation] = useState('');
+    const [position, setPosition] = useState(null);
+    const [error, setError] = useState(null);
 
     const liveFilter = (e) => {
         setSearchText(e.target.value);
@@ -26,55 +27,47 @@ const Body = () => {
         else setFilteredRestaurants(allRestaurants);
     }
 
-    function setLocation(){
-        if (navigator.geolocation) {
+    useEffect(() => {
+        if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition((position) => {
+                setPosition(position);
                 fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=${position?.coords?.latitude}&lng=${position?.coords?.longitude}&page_type=DESKTOP_WEB_LISTING`)
                     .then(response => response.json())
                     .then(data => {
                         setAllRestaurants(data?.data?.cards[2]?.data?.data?.cards);
                         setFilteredRestaurants(data?.data?.cards[2]?.data?.data?.cards);
                     })
+            },(err)=>{
+                setError(err);
             })
-        }
-    }
-
-    useEffect(() => {
-        setLocation();
+        } else setError("Geolocation is not available in this browser");
     }, [])
 
     const isOnline = useOnline();
     if (!isOnline) return <Offline />;
-    console.log(window.innerWidth);
-    const getlocation = () => {
-        console.log("pressed");
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition((position)=>{
-                setlocation(position);
-            })
-        }
+
+    if(error){
+        return <div>{error}</div>
     }
 
-    return (
-        <>
-            <div className="search-box">
-                <input type="text" name="search" id="search" value={searchText} onChange={e => liveFilter(e)} placeholder="Search for restaurants and food" />
-            </div>
-            {
-                location?<h1>{location.coords.latitude}</h1>: <h1></h1>
-            } 
-             
-            <div className="body">
-                {
-                    allRestaurants?.length === 0 ? (<Shimmer />) :
-                        filteredRestaurants?.length > 0 ? (filteredRestaurants.map(restaurant => {
-                            return <Link to={"/restaurant/" + restaurant.data.id} key={restaurant.data.id}><RestaurantCard {...restaurant.data} /></Link>
-                        })) : (<h2 className="no-results">We didn't find any matching results for "{searchText}"</h2>)
-                }
-            </div>
-            <button onClick={()=>getlocation()}>click me</button>
-        </>
-    )
+    if (position) {
+        return (
+            <>
+                <div className="search-box">
+                    <input type="text" name="search" id="search" value={searchText} onChange={e => liveFilter(e)} placeholder="Search for restaurants and food" />
+                </div>
+                <div className="body">
+                    {
+                        allRestaurants?.length === 0 ? (<Shimmer />) :
+                            filteredRestaurants?.length > 0 ? (filteredRestaurants.map(restaurant => {
+                                return <Link to={"/restaurant/" + restaurant.data.id} key={restaurant.data.id}><RestaurantCard {...restaurant.data} /></Link>
+                            })) : (<h2 className="no-results">We didn't find any matching results for "{searchText}"</h2>)
+                    }
+                </div>
+            </>
+        )
+    }
+    return <Shimmer />
 }
 
 export default Body;
